a6222803157fa00c8b4feb16c7c6592bcf3d2d57
Will Melani <will_melani@gap.com>
2015-06-11 13:11:02 -0700

    add dummy post request untested

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ app.get('/', function(request, response) {
   response.send('so sad today');
 });
 
+app.post('/', function(request, response) {
+  var body = JSON.parse(request.body);
+
+	var obj = {
+		  "version": "1.0",
+		  "sessionAttributes": {
+		  },
+		  "response": {
+		    "outputSpeech": {
+		      "type": "PlainText",
+		      "text": "I am so sad."
+		    },
+		    "card": {
+		      "type": "Simple",
+		      "title": "Sadness",
+		      "content": "So sad"
+		    }
+		    "shouldEndSession": true
+		  }
+		};
+	response.send(obj);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
