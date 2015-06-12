db10f7f036daa9d68f8da086f2a0818d38384dcd
Will Melani <will_melani@gap.com>
2015-06-11 20:52:05 -0700

    update to use promises

--- controllers/alexa.js
+++ controllers/alexa.js
@@ -1,31 +1,13 @@
 "use strict";
 var Twitter = require('twitter');
 var ent = require('ent');
+var Promise = require('mpromise');
 function alexaGetRequest(req,response,done){
-    var obj = {
-        "version": "1.0",
-        "sessionAttributes": {
-        },
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "I am so sad."
-            },
-            "card": {
-                "type": "Simple",
-                "title": "Sadness",
-                "content": "So sad"
-            },
-            "shouldEndSession": true
-        }
-    };
-    response.send(obj);
-
+    handleRequest(req,response,done);
 }
 
-
-function alexaPostRequest(req,response,done){
-    getRandomTweet(function(text){
+function handleRequest(req,response,done){
+    getRandomTweet().then(function(text){
         var obj = {
             "version": "1.0",
             "sessionAttributes": {
@@ -46,18 +28,24 @@ function alexaPostRequest(req,response,done){
         response.send(obj);
     });
 }
+function alexaPostRequest(req,response,done){
+    handleRequest(req,response,done);
+}
 
-function getRandomTweet(callback){
+function getRandomTweet(){
     var client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
         access_token_key: process.env.TWITTER_ACCESS_TOKEN,
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
+    var promise = new Promise;
     client.get('statuses/user_timeline', {screen_name : "sosadtoday", exclude_replies : true}, function(err,items){
         var item = items[Math.floor(Math.random()*items.length)];
-        callback(ent.decode(item.text));
-    })
+        promise.resolve(err,ent.decode(item.text));
+    });
+
+    return promise;
 }
 
 module.exports = exports;
