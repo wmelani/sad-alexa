e031e2162bcca2d29b84040a65d62753d2e1c633
Will Melani <will_melani@gap.com>
2015-06-11 20:02:51 -0700

    Alexa is sad today

--- controllers/alexa.js
+++ controllers/alexa.js
@@ -1,5 +1,6 @@
 "use strict";
-
+var Twitter = require('twitter');
+var ent = require('ent');
 function alexaGetRequest(req,response,done){
     var obj = {
         "version": "1.0",
@@ -21,27 +22,43 @@ function alexaGetRequest(req,response,done){
     response.send(obj);
 
 }
+
+
 function alexaPostRequest(req,response,done){
-    var obj = {
-        "version": "1.0",
-        "sessionAttributes": {
-        },
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "I am so sad."
+    getRandomTweet(function(text){
+        var obj = {
+            "version": "1.0",
+            "sessionAttributes": {
             },
-            "card": {
-                "type": "Simple",
-                "title": "Sadness",
-                "content": "So sad"
-            },
-            "shouldEndSession": true
-        }
-    };
-    response.send(obj);
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": text
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Sadness",
+                    "content": text
+                },
+                "shouldEndSession": true
+            }
+        };
+        response.send(obj);
+    });
 }
 
+function getRandomTweet(callback){
+    var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+    client.get('statuses/user_timeline', {screen_name : "sosadtoday", exclude_replies : true}, function(err,items){
+        var item = items[Math.floor(Math.random()*items.length)];
+        callback(ent.decode(item.text));
+    })
+}
 
 module.exports = exports;
 exports.alexaGetRequest = alexaGetRequest;
